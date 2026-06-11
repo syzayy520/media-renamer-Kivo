6eabffebe61ea5e8b71762a823f87215a4d28b66
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 15:26:38 +0800

    refactor(tmdb): export preview card

--- src/pages/preview/tmdb/index.ts
+++ src/pages/preview/tmdb/index.ts
@@ -1,5 +1,6 @@
 export { CandidateList } from './CandidateList';
 export { CandidateRow } from './CandidateRow';
+export { ScrapePreviewCard } from './ScrapePreviewCard';
 export { SearchTypeButton } from './SearchTypeButton';
 export { TmdbErrorCard, TmdbLoadingCard } from './TmdbStatusCards';
 export { TMDB_IMAGE_BASE, isMovieCandidate } from './tmdbInspectorTypes';
