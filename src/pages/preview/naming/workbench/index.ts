732f2a48a569614cca8f85b9f1d041e43a62b529
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 18:43:12 +0800

    refactor(preview): export naming workbench hook

--- src/pages/preview/naming/workbench/index.ts
+++ src/pages/preview/naming/workbench/index.ts
@@ -1,6 +1,7 @@
 export { NamingWorkbench } from './NamingWorkbench';
 export { PreviewNamingWorkbenchPanel } from './PreviewNamingWorkbenchPanel';
 export { PreviewNamingWorkbenchSidebar } from './PreviewNamingWorkbenchSidebar';
+export { usePreviewNamingWorkbench } from './usePreviewNamingWorkbench';
 export {
   applyPresetToWorkbench,
   applyRuleToWorkbench,
