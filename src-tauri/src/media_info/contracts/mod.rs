c08e29951e718796b56d5501e34baa81e719b428
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 13:08:19 +0800

    feat(media-info): add media info contracts root

---
+++ src-tauri/src/media_info/contracts/mod.rs
@@ -0,0 +1,7 @@
+pub mod media_info_probe_input;
+pub mod media_info_probe_output;
+pub mod normalized_media_info;
+
+pub use media_info_probe_input::MediaInfoProbeInput;
+pub use media_info_probe_output::MediaInfoProbeOutput;
+pub use normalized_media_info::{NormalizedAudioInfo, NormalizedMediaInfo, NormalizedVideoInfo};
