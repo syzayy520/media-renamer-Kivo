3489f97ec41a5e9f7be4627748ebf21fef3db92d
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 15:40:05 +0800

    refactor(tmdb): export empty selection card

--- src/pages/preview/tmdb/index.ts
+++ src/pages/preview/tmdb/index.ts
@@ -2,6 +2,7 @@ export { CandidateList } from './CandidateList';
 export { CandidateRow } from './CandidateRow';
 export { ScrapePreviewCard } from './ScrapePreviewCard';
 export { SearchTypeButton } from './SearchTypeButton';
+export { TmdbEmptySelectionCard } from './TmdbEmptySelectionCard';
 export { TmdbSearchCard } from './TmdbSearchCard';
 export { TmdbErrorCard, TmdbLoadingCard } from './TmdbStatusCards';
 export { TMDB_IMAGE_BASE, isMovieCandidate } from './tmdbInspectorTypes';
