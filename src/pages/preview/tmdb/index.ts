4cf4b3c489e8370ce6f32d94e5167c1372d725f4
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 15:06:08 +0800

    refactor(tmdb): export inspector contracts

---
+++ src/pages/preview/tmdb/index.ts
@@ -0,0 +1,8 @@
+export { TMDB_IMAGE_BASE, isMovieCandidate } from './tmdbInspectorTypes';
+export type {
+  CandidateListProps,
+  CandidateRowProps,
+  ScrapePreviewCardProps,
+  TmdbInspectorPanelProps,
+  TmdbManualMediaType,
+} from './tmdbInspectorTypes';
