5f780dfe9cd0acd92cb83724776e3292518a82e8
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 16:05:00 +0800

    refactor(tmdb): export disabled banner

--- src/pages/preview/tmdb/index.ts
+++ src/pages/preview/tmdb/index.ts
@@ -2,6 +2,7 @@ export { CandidateList } from './CandidateList';
 export { CandidateRow } from './CandidateRow';
 export { ScrapePreviewCard } from './ScrapePreviewCard';
 export { SearchTypeButton } from './SearchTypeButton';
+export { TmdbDisabledBanner } from './TmdbDisabledBanner';
 export { TmdbEmptySelectionCard } from './TmdbEmptySelectionCard';
 export { TmdbSearchCard } from './TmdbSearchCard';
 export { TmdbErrorCard, TmdbLoadingCard } from './TmdbStatusCards';
