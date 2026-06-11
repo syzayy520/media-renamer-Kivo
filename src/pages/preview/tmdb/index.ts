04d3b4b51fe68882430f0d4deb1ca323b5784b49
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 15:10:37 +0800

    refactor(tmdb): export candidate list component

--- src/pages/preview/tmdb/index.ts
+++ src/pages/preview/tmdb/index.ts
@@ -1,3 +1,4 @@
+export { CandidateList } from './CandidateList';
 export { CandidateRow } from './CandidateRow';
 export { TMDB_IMAGE_BASE, isMovieCandidate } from './tmdbInspectorTypes';
 export type {
